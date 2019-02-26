d4ed0039d4a089f986d062f36de906c9e5f5847d
Jin Kim <jkim1665@gmail.com>
2019-02-26 17:01:43 -0500

    first commit

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
-var section = $("questionWillGoHere");
+var section = $("#questionHere");
 
+//questions array
 var questions = [{
     question: "What does MVP in project Management?",
     answers: ["Most Valuable Player", "Most Valuable Person", "Minimum Viable product", "Maximum viable product"],
@@ -22,67 +23,141 @@ var questions = [{
     correctAnswer: "XML"
 }];
 
+//var will hold time and other will hold instructuions for the game.
+
 var timer;
 
-var startGame = {
+var game = {
 
-    counter:120,
-    correct: 0,
-    incorrect: 0,
+// Variable that will hold timer, correct, and inside global var.
+  correct: 0,
+  incorrect: 0,
+  counter: 120,
 
-    countdown: function() {
-        game.counter--;
-        $()
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      game.done();
+    }
+  },
+
+  start: function() {
+    timer = setInterval(game.countdown, 1000);
+
+    $("#background").prepend("<h2>Time Remaining: <span id='counter-number'>120</span> Seconds</h2>");
+
+    $("#start").remove();
+
+    for (var i = 0; i < questions.length; i++) {
+      section.append("<h2>" + questions[i].question + "</h2>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        section.append("<input type='radio' name='question-" + i +
+        "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
+      }
     }
 
-}
+    section.append("<button id='done'>Done</button>");
+  },
+
+  done: function() {
+
+    $.each($("input[name='question-0']:checked"), function() {
+      if ($(this).val() === questions[0].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-1']:checked"), function() {
+      if ($(this).val() === questions[1].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-2']:checked"), function() {
+      if ($(this).val() === questions[2].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-3']:checked"), function() {
+      if ($(this).val() === questions[3].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-4']:checked"), function() {
+      if ($(this).val() === questions[4].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-5']:checked"), function() {
+      if ($(this).val() === questions[5].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-6']:checked"), function() {
+      if ($(this).val() === questions[6].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-7']:checked"), function() {
+      if ($(this).val() === questions[7].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    this.result();
+
+  },
+
+  result: function() {
+
+    clearInterval(timer);
+
+    $("#background h2").remove();
+
+    section.html("<h2>How'd you do?</h2>");
+    section.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    section.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    section.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+  }
+};
+
+// CLICK EVENTS
+
+$(document).on("click", "#start", function() {
+  game.start();
+});
 
 
-var quiz = new Quiz(questions);
-
-populate();
-
-function Quiz(questions) {
-    this.score = 0;
-    this.questions = questions;
-    this.questionIndex = 0;
-}
-
-Quiz.prototype.getQuestionIndex = function() {
-    return this.questions(this.questionIndex);
-}
-
-Quiz.prototype.isEnded = function () {
-    return this.questions.length === this.questionIndex;
-}
-
-Quiz.prototype.guess = function(answer) {
-    this.questionIndex++;
-
-    if(this.getQuestionIndex().correctAnswer(answer)) {
-        this.score++;
-        }
-    }
-
-
-
-    function Question (text, choices, answer) {
-        this.text = text;
-        this.choices = choices;
-        this.answer = answer;
-    }
-    
-    Question.prototype.correctAnswer = function(choice) {
-        return choice === this.answer;
-    }
-    
-    function populate() {
-        if(quiz.isEnded()) {
-            // showScores() ;
-        }
-        else {
-            //show question
-            var element = document.getElementById("question");
-            element.innerHTML = quiz.getQuestionIndex().text;
-        }
-    }
+$(document).on("click", "#done", function() {
+  game.done();
+});
